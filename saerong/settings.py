c1a73e02b4219664785d6165dc6ba28896669927
Sangki Jeon <sangki1298@gmail.com>
2025-04-04 02:35:01 +0900

    변경

--- saerong/settings.py
+++ saerong/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 from django.core.exceptions import ImproperlyConfigured
 import environ
+Env = environ.Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
